c6d4cd3dde6cf7b2897d1feea38258df96444a8a
TESTSIGNUP123 <2318376964@qq.com>
2024-06-20 23:55:06 +0800

    first commit

--- lisp/init-code.el
+++ lisp/init-code.el
@@ -1,53 +1,14 @@
 ;;Company-MODE
-(add-hook 'after-init-hook 'global-company-mode)
-(require 'company-box)
-(add-hook 'company-mode-hook 'company-box-mode)
- (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
-  (setq company-tooltip-align-annotations t)
-  (setq company-idle-delay 0.0)
-  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
-  (setq company-selection-wrap-around t)
- (setq company-transformers '(company-sort-by-occurrence))  ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
+;(add-hook 'after-init-hook 'global-company-mode)
+;(require 'company-box)
+;(add-hook 'company-mode-hook 'company-box-mode)
+; (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+;  (setq company-tooltip-align-annotations t)
+;  (setq company-idle-delay 0.0)
+;  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+;  (setq company-selection-wrap-around t)
+; (setq company-transformers '(company-sort-by-occurrence))  ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
 
-;；Lsp
-;(require 'lsp-mode)
-;(require 'lsp-ui)
-;(require 'lsp-ui-imenu)
-
-;(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-;(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-workspace-restart);
-;(define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
-;(define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
-
-;(setq lsp-ui-sideline-enable t)
-;(setq lsp-ui-doc-enable t)
-;(setq lsp-ui-peek-enable t)
-;(setq lsp-ui-peek-always-show t)
-;;langers
-;(add-hook 'c++-mode-hook #'lsp)
-;(add-hook 'js-mode #'lsp)
-;(add-hook 'python-mode-book #'lsp)
-;(add-hook 'typescript-mode #'lsp)
-
-;(global-flycheck-mode)
-;(setq truncate-lines nil) 
-;(prog-mode . flycheck-mode)
-
-;;lsp-java
-;(require 'lsp-java)
-;(add-hook 'java-mode-hook #'lsp)
-;(add-hook 'java-mode-hook (lambda ()
-;                            (setq c-default-style "java")
- ;                           (setq c-basic-offset 4)
-  ;                          (display-line-numbers-mode t)
-   ;                         (gradle-mode 1)
-    ;                        ))
 
 ;;ts;
 ;(defun my-setup-dap-node ()
@@ -68,9 +29,13 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 (add-hook 'elisp-byte-code-mode-hook 'lsp)
-;lsp-bridge
-;(require 'yasnippet)
-;(yas-global-mode 1)
+(add-hook 'css-mode-hook 'lsp)
+(add-hook 'go-mode-hook 'lsp)
+(add-hook java-mode-hook 'lsp)
+
+					;lsp-bridge
+(require 'yasnippet)
+(yas-global-mode 1)
 
 ;(require 'lsp-bridge)
 ;(global-lsp-bridge-mode)
@@ -79,7 +44,6 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/lsp-bridge")
 (require 'yasnippet)
 (yas-global-mode 1)
-
 (require 'lsp-bridge)
 (global-lsp-bridge-mode)
 
@@ -100,7 +64,7 @@
 ;; Last we customize `lsp-bridge-get-single-lang-server-by-project' to return `deno' lsp server name.
 ;; I recommand you write some code to compare project-path or file-path, return `deno' only if match target path.
 (setq lsp-bridge-get-single-lang-server-by-project
-      (lambda (project-path file-path)
+     (lambda (project-path file-path)
 	(when (or (string-suffix-p ".ts" file-path)
 		  (string-suffix-p ".tsx" file-path))
 	  "deno")))
